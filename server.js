2e096b1178f1f7aed65e46b06c864dfddcce4c20
Dalet <dmolet@outlook.com>
2019-06-19 12:51:03 +0200

    add sockets handler

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const socketio = require('socket.io');
 const mongoose = require('mongoose');
+const sockets = require('./sockets');
 
 var app = express();
 var server = app.listen(8080);
 
 var io = socketio(server);
+sockets(io);
 
 app.use(express.static('./static'));
