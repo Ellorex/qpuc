bbfa6318cd766669322181b6df9f213c9e8533f5
Lea <lea.rerolle@gmail.com>
2019-06-21 17:53:47 +0200

    send id answer OK

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,12 @@
 var socket = io("/play", { transports: ['websocket'], upgrade: false });
+var questionTitle = document.getElementById('questionTitle');
+var answersList = document.getElementById('answersList');
 var playerName = null;
+var listPlayer = document.getElementById('listPlayer');
+var submitAnswer = document.getElementById('submitAnswer');
+var radios = document.getElementsByName("radio");
+var verifradios = false;
+var message = document.getElementById('message');
 
 let state = null;
 if (document.getElementById('btnPlay')) {
@@ -12,13 +19,12 @@ if (document.getElementById('btnPlay')) {
 socket.on('joinGame', (success) => {
     if (success) {
         document.getElementById('player-name-form-row').style.display = 'none';
-        document.getElementById('main-container').style.display = 'block';
-        console.log("joined the game");
+        document.getElementById('main-container').style.display = 'flex';
     } else {
-        console.log("Player name taken");
         var alert = document.createElement('div');
         alert.classList.add('alert');
         alert.classList.add('alert-danger');
+        alert.classList.add('m-t-30');
         alert.setAttribute('id', 'alert');
         alert.innerText = 'Ce joueur existe déjà. Veuillez choisir un autre nom.';
         document.getElementById('player-name-form-row').append(alert);
@@ -31,17 +37,62 @@ socket.on("gameState", s => {
     updateView(state);
 });
 
-function displayPlayerList (state) {
-    var listPlayer = document.getElementById('listPlayer');
+function displayPlayerList(state) {
     state.players.forEach(player => {
         var li = document.createElement('li');
         li.classList.add('player');
-        li.setAttribute('id', 'player_'+player.name);
+        li.setAttribute('id', 'player_' + player.name);
         li.innerText = player.name;
         listPlayer.appendChild(li);
     });
 }
 
 function updateView(state) {
+    listPlayer.innerHTML = "";
     displayPlayerList(state);
 }
+
+socket.on('selectExistingQuestion', data => {
+    var answers_str = "";
+    questionTitle.innerHTML = data.title;
+    var answers = data.answers;
+
+    for (var i = 0; i < answers.length; i++) {
+
+        answers_str += "<div class='col-6 form-check'><input class='form-check-input' type='radio' name='radio' id='" + answers[i]._id + "'> <label class='form-check-label' for='" + answers[i]._id + "'>" + answers[i].title + "</label></div>";
+    }
+    answersList.innerHTML = answers_str;
+
+    submitAnswer.addEventListener('click', (e) => {
+        message.innerHTML = "";
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                idAnswer = radios[i].id
+                verifradios = true;
+                break;
+            }
+        }
+        if (verifradios) {
+            for(i=0; i< answers.length; i ++) {
+                var indexAnswer;
+                if(answers[i]._id == idAnswer) {
+                    indexAnswer = i;
+                    console.log(indexAnswer)
+                    if(answers[i].correct ==true) {
+                        message.innerHTML = "<div class='alert alert-success text-center m-t-30'>Bonne réponse</div>"
+                    } else {
+                        message.innerHTML = "<div class='alert alert-warning text-center m-t-30'>Mauvaise réponse</div>"
+                    }
+                }
+            }
+            socket.emit('sendAnswer', indexAnswer);
+        } else {
+            message.innerHTML = "<div class='alert alert-danger text-center m-t-30'>Vous n'avez pas entré de réponse</div>"
+        }
+    });
+});
+
+
+
+
+
